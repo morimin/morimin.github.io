6d3fee10ea85e2a7227117d633df582d2e0a6ee0
morimin <31561404+morimin@users.noreply.github.com>
2024-07-01 22:03:25 +0900

    Update notion-import.js on 1st of July, 2024

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -15,7 +15,7 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
   // ensure directory exists
-  const root = `docs`
+  const root = `_posts`
 
   const databaseId = process.env.DATABASE_ID
   const response = await notion.databases.query({
@@ -28,76 +28,55 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
             equals: true,
           },
         },
-        {
-          property: "배포",
-          checkbox: {
-            equals: true,
-          },
-        },
       ],
     },
   })
   for (const r of response.results) {
     const id = r.id
-
-    // 최상위폴더
-    let upUpFolder = ""
-    let pUpUpFolder = r.properties?.["최상위폴더"]?.["rich_text"]
-    if (pUpUpFolder) {
-      upUpFolder = pUpUpFolder[0]?.["plain_text"]
+    
+    // date
+    let date = moment(r.created_time).format("YYYY-MM-DD");
+    let pdate = r.properties?.["날짜"]?.["date"]?.["start"];
+    if (pdate) {
+      date = moment(pdate).format("YYYY-MM-DD");
     }
-
-    // 상위폴더
-    let upFolder = ""
-    let pUpFolder = r.properties?.["상위폴더"]?.["rich_text"]
-    if (pUpFolder) {
-      upFolder = pUpFolder[0]?.["plain_text"]
+    // title
+    let title = id;
+    let ptitle = r.properties?.["게시물"]?.["title"];
+    if (ptitle?.length > 0) {
+      title = ptitle[0]?.["plain_text"];
     }
-
-    // 순번
-    let navOrder = r.properties?.["순번"]?.["number"] || ""
-
-    // 제목
-    let title = id
-    let pTitle = r.properties?.["제목"]?.["title"]
-    if (pTitle?.length > 0) {
-      title = pTitle[0]?.["plain_text"]
+    // tags
+    let tags = [];
+    let ptags = r.properties?.["태그"]?.["multi_select"];
+    for (const t of ptags) {
+      const n = t?.["name"];
+      if (n) {
+        tags.push(n);
+      }
     }
-
-    // 메인
-    let hasChild = r.properties?.["메인"]?.["checkbox"] || false
-
-    // 작성일
-    let date = moment(r.created_time).tz("Asia/Seoul").format("YYYY-MM-DD HH:mm")
-
+    // categories
+    let cats = [];
+    let pcats = r.properties?.["카테고리"]?.["multi_select"];
+    for (const t of pcats) {
+      const n = t?.["name"];
+      if (n) {
+        cats.push(n);
+      }
+    }
+    
     let header = `---
-layout: default
 title: ${title}
-has_children: ${hasChild}
-last_modified_date: ${date}`
-
-    if (navOrder) {
-      header += `
-nav_order: ${navOrder}`
-    }
-
-    if (hasChild) {
-      if (upFolder) {
-        header += `
-parent: ${upUpFolder}`
-      }
-    } else {
-      header += `
-grand_parent: ${upUpFolder}`
-      if (upFolder) {
-        header += `
-parent: ${upFolder}`
-      }
-    }
-    header += `
+excerpt : ""
+header : ""
+categories : 
+    - ${cats}
+tags : 
+    - ${tags}
+last_modified_at : ${date}
 ---`
-
-    const folderPath = upFolder ? `${root}/${upUpFolder}/${upFolder}` : `${root}/${upUpFolder}`
+    
+    const folderPath = ${root}
     fs.mkdirSync(folderPath, { recursive: true })
 
     const mdBlocks = await n2m.pageToMarkdown(id)
@@ -105,7 +84,7 @@ parent: ${upFolder}`
 
 
     //writing to file
-    const fTitle = `${title}.md`
+    const fTitle = `${date}-${title}.md`
     fs.writeFile(path.join(folderPath, fTitle), header + body, (err) => {
       if (err) {
         console.log(err)
